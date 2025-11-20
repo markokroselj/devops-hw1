cc5eaecb46d6e03849801963d6b3227b5a02f3cf
markokroselj <kroselj.marko123@gmail.com>
2025-11-20 13:47:23 +0100

    Add new location

--- app/db/dbsetup.sql
+++ app/db/dbsetup.sql
@@ -55,4 +55,6 @@ alter table Vehicle add constraint FK_is_at foreign key (Location_ID)
       
 /*Insert initial types and locations */     
 insert into Type (Type) values ('Ship'), ('Booster');
-insert into Location (Location_name, Location_link) values ('Production site', 'https://www.openstreetmap.org/export/embed.html?bbox=-97.189444%2C25.985445%2C-97.187444%2C25.987445&layer=mapnik&marker=25.986445%2C-97.188444'), ('Launch site', 'https://www.openstreetmap.org/export/embed.html?bbox=-97.157568%2C25.996227%2C-97.155568%2C25.998227&layer=mapnik&marker=25.997227%2C-97.156568');
+insert into Location (Location_name, Location_link) values ('Production Site', 'https://www.openstreetmap.org/export/embed.html?bbox=-97.189444%2C25.985445%2C-97.187444%2C25.987445&layer=mapnik&marker=25.986445%2C-97.188444'),
+ ('Launch Site', 'https://www.openstreetmap.org/export/embed.html?bbox=-97.157568%2C25.996227%2C-97.155568%2C25.998227&layer=mapnik&marker=25.997227%2C-97.156568'),
+ ("Massey's Test Site", "https://www.openstreetmap.org/export/embed.html?bbox=-97.252483%2C25.948462%2C-97.248483%2C25.952462&layer=mapnik&marker=25.950462%2C-97.250483");
